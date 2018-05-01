c82a8d6e5a81f1d572752326b16555db25531846
tronxi <tronxi97@gmail.com>
2018-05-02 01:53:01 +0200

    prueba solo select

--- baseDatos.php
+++ baseDatos.php
@@ -1,8 +1,8 @@
 <?php
   $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
   mysqli_select_db($con, "pruebaBaseDatos");
-  $query = "insert into tablaPrueba (nombre) values ('.$_POST['$nombre'].')";
-  mysqli_query($con, $query);
+  /*$query = "insert into tablaPrueba (nombre) values ('.$_POST['$nombre'].')";
+  mysqli_query($con, $query);*/
   $query = "select * from tablaPrueba";
   $resul = mysqli_query($con, $query);
   while($fila = mysqli_fetch_array($resul))
