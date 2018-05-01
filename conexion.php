8e4260486bde8987a9f2ef75e33354b3da3cbe63
tronxi <tronxi97@gmail.com>
2018-05-02 01:44:46 +0200

    formulario

--- conexion.php
+++ conexion.php
@@ -4,10 +4,15 @@
   </head>
   <body>
     <h1>Conexion BD</h1>
+    <form method="POST" action="conexion.php">
+      nombre<br />
+      <input type="TEXT" NAME="nombre" />
+      <input type="SUBMIT" />
+    </form>
     <?php
       $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
       mysqli_select_db($con, "pruebaBaseDatos");
-      $query = "insert into tablaPrueba (nombre) values ('pedro')";
+      $query = "insert into tablaPrueba (nombre) values ('$nombre')";
       mysqli_query($con, $query);
       $query = "select * from tablaPrueba";
       $resul = mysqli_query($con, $query);
