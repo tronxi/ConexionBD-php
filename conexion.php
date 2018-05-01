c46c6084180329b9bcabf2865feff571f386d861
tronxi <tronxi97@gmail.com>
2018-05-02 01:21:42 +0200

    holaMundo conexion

---
+++ conexion.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Conexion BD</title>
+  </head>
+  <body>
+    <h1>Conexion BD</h1>
+    <?php
+      echo "<p>
+        hola mundo
+      </p>";
+    ?>
+  </body>
+</html>
