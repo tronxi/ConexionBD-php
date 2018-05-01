e4fb49c054991f1871baa8c448fb941a8cbee808
tronxi <tronxi97@gmail.com>
2018-05-02 01:58:34 +0200

    prueba

--- baseDatos.php
+++ baseDatos.php
@@ -3,6 +3,8 @@
   mysqli_select_db($con, "pruebaBaseDatos");
   /*$query = "insert into tablaPrueba (nombre) values ('.$_POST['$nombre'].')";
   mysqli_query($con, $query);*/
+  echo "nombre de variable". $_POST['$nombre'] ."texto";
+  echo "<br />";
   $query = "select * from tablaPrueba";
   $resul = mysqli_query($con, $query);
   while($fila = mysqli_fetch_array($resul))
