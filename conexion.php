b95022b3478bf3a427131428933906e7297f1bfb
tronxi <tronxi97@gmail.com>
2018-05-02 01:28:47 +0200

    insertar pedro

--- conexion.php
+++ conexion.php
@@ -6,6 +6,11 @@
     <h1>Conexion BD</h1>
     <?php
       echo "<p>hola mundo</p>";
+      $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
+      mysqli_select_db($con, "pruebaBaseDatos");
+      $query = "insert into tablaPrueba (nombre) values ('pedro')";
+      mysqli_query($con, $query);
+      mysqli_close($con);
     ?>
   </body>
 </html>
