28eec7c6c60ffb0b0cc43f4e776e058ffcb14ceb
tronxi <tronxi97@gmail.com>
2018-05-02 01:32:27 +0200

    eliminado hola mundo

--- conexion.php
+++ conexion.php
@@ -5,7 +5,6 @@
   <body>
     <h1>Conexion BD</h1>
     <?php
-      echo "<p>hola mundo</p>";
       $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
       mysqli_select_db($con, "pruebaBaseDatos");
       $query = "insert into tablaPrueba (nombre) values ('pedro')";
