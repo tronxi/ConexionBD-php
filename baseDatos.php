8f40c513e1a981c92dd2deb3d9cc6fbf71caeb26
tronxi <tronxi97@gmail.com>
2018-05-02 02:03:59 +0200

    insertar desde formulario

--- baseDatos.php
+++ baseDatos.php
@@ -1,15 +1,13 @@
 <?php
   $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
   mysqli_select_db($con, "pruebaBaseDatos");
-  /*$query = "insert into tablaPrueba (nombre) values ('.$_POST['$nombre'].')";
-  mysqli_query($con, $query);*/
-  echo "nombre de variable". $_POST['nombre'] ."texto";
-  echo "<br />";
+  $query = "insert into tablaPrueba (nombre) values ('.$_POST['nombre'].')";
+  mysqli_query($con, $query);
   $query = "select * from tablaPrueba";
   $resul = mysqli_query($con, $query);
   while($fila = mysqli_fetch_array($resul))
   {
-    echo "id: ".$fila['id']."nombre: ".$fila['nombre'];
+    echo "id: ".$fila['id']." nombre: ".$fila['nombre'];
     echo "<br />";
   }
   mysqli_close($con);
