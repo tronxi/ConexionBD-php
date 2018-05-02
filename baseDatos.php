b73f9730db9f076e1008237532c2801e70b41889
tronxi <tronxi97@gmail.com>
2018-05-02 02:07:37 +0200

    solo insertar

--- baseDatos.php
+++ baseDatos.php
@@ -1,14 +1,14 @@
 <?php
   $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
   mysqli_select_db($con, "pruebaBaseDatos");
-  $query = "insert into tablaPrueba (nombre) values ('.$_POST['nombre'].')";
+  $query = "insert into tablaPrueba (nombre) values ('. $_POST['nombre'] .')";
   mysqli_query($con, $query);
-  $query = "select * from tablaPrueba";
+/*  $query = "select * from tablaPrueba";
   $resul = mysqli_query($con, $query);
   while($fila = mysqli_fetch_array($resul))
   {
-    echo "id: ".$fila['id']." nombre: ".$fila['nombre'];
+    echo "id: ".$fila['id']."nombre: ".$fila['nombre'];
     echo "<br />";
-  }
+  }*/
   mysqli_close($con);
 ?>
