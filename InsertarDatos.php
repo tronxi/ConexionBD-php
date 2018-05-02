6d99e6f0d54b66b1e6cd5b135aeb71d9d3d2e471
tronxi <tronxi97@gmail.com>
2018-05-02 10:33:28 +0200

    cambio con dos ficheros php

---
+++ InsertarDatos.php
@@ -0,0 +1,7 @@
+<?php
+  $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
+  mysqli_select_db($con, "pruebaBaseDatos");
+  $query = "insert into tablaPrueba (nombre) values ('".$_POST['nombre']."')";
+  mysqli_query($con, $query);
+  mysqli_close($con);
+?>
--- MostrarDatos.php
+++ MostrarDatos.php
@@ -1,8 +1,6 @@
 <?php
   $con = mysqli_connect("192.168.0.5:3306", "tronxi", "tronxi97");
   mysqli_select_db($con, "pruebaBaseDatos");
-  $query = "insert into tablaPrueba (nombre) values ('".$_POST['nombre']."')";
-  mysqli_query($con, $query);
   $query = "select * from tablaPrueba";
   $resul = mysqli_query($con, $query);
   $json = array();
