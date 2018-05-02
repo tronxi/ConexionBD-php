2446a45fb0f95e554679e318cd08adfa533a1638
tronxi <tronxi97@gmail.com>
2018-05-02 10:52:52 +0200

    prueba AJAX

---
+++ javascprit/pruebaAJAX.js
@@ -0,0 +1,35 @@
+addEventListener('load', inicializarEventos, false);
+function inicializarEventos()
+{
+  var mostrar = document.getElementByID("mostrar");
+  mostrar.addEventListener('click', mostrarDatos, false);
+}
+
+function mostrarDatos(e)
+{
+  e.preventDefault();
+  var url = e.target.getAttribute('href');
+  cargarDatos(url);
+}
+
+var conexion;
+function cargarDatos(url)
+{
+  conexion = new XMLHttpRequest(url);
+  conexion.onreadystatechange = procesarEventos;
+  conexion.open("GET", url, true);
+  conexion.send();
+}
+
+function procesarEventos()
+{
+  var datos = document.getElementById("detalles");
+  if(conexion.readyState == 4)
+  {
+    datos.innerHTML = conexion.responseText;
+  }
+  else
+  {
+    datos.innerHTML = 'Cargando...';
+  }
+}
